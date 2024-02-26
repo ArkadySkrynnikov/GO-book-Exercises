6424580923756c3854d7bc8d56a8c418557b40d9
ArkadySkrynnikov <catmaster1488@gmail.com>
2024-02-26 20:27:16 +0700

    add 1.8, 1-9 exercise

---
+++ Chapter 1/1-9/e1.9.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	urlEnter := os.Args[1:]
+
+	for _, url := range urlEnter {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(0)
+		}
+
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+		fmt.Println("Status: ", resp.Status)
+	}
+}
